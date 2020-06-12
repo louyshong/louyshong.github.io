56809110add1d8e35f56d9b677467c826921c939
Tuck Kong Hong <louyshong1998@gmail.com>
2020-06-12 17:38:27 +0100

    lead animation

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const flightPath = {
-    curviness: 1.5, 
+    curviness: 1.25, 
     autoRotate: true, 
     values: [
-        {x: 200, y: 30}, 
-        {x: 100, y: 80}, 
-        {x: 80, y: 200}, 
-        {x: 100, y: 150},
-        {x: 60, y: 400},
-        {x: 170, y: 500},
+        {x: -0.2 * $("#rightcol").width(), y: 0.1 * $("#rightcol").height()}, 
+        {x: -0.5 * $("#rightcol").width(), y: 0.2 * $("#rightcol").height()}, 
+        {x: -0.3 * $("#rightcol").width(), y: 0.6 * $("#rightcol").height()}, 
+        {x: -0.3 * $("#rightcol").width(), y: 0.5 * $("#rightcol").height()}, 
+        {x: -0.5 * $("#rightcol").width(), y: 0.9 * $("#rightcol").height()}, 
         {x: $("#rightcol").width(), y: $("#rightcol").height()}
     ]
 };
@@ -15,5 +14,18 @@ const flightPath = {
 const tween = new TimelineLite();
 
 tween.add(
-    TweenLite.to("#leaf", 8, {bezier: flightPath, ease: Power1.easeInOut})
+    TweenLite.to("#leaf", 10, {bezier: flightPath, ease: Power1.easeInOut})
 );
+
+const controller = new ScrollMagic.Controller();
+
+var hook = 0.28; 
+
+// if (window.innerHeight < ) {
+//     hook = 0.4;    
+// }
+
+const scene = new ScrollMagic.Scene(
+    {triggerElement: "#rightcol",
+     duration: $("#rightcol").height(),
+     triggerHook: hook}).setTween(tween).addTo(controller);
